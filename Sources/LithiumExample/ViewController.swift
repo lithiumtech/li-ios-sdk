b7e965615bf4808136cadf55a4184b5d277bdf58
Shekhar Dahore <shekhar.dahore@lithium.com>
2018-06-19 09:59:31 +0530

    Updated license.

--- Sources/LithiumExample/ViewController.swift
+++ Sources/LithiumExample/ViewController.swift
@@ -1,21 +1,25 @@
+// Copyright 2018 Lithium Technologies
 //
-//  ViewController.swift
-//  LithiumExample
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Shekhar Dahore on 5/30/18.
-//  Copyright © 2018 Shek. All rights reserved.
+// http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import UIKit
 import LiUIComponents
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -25,7 +29,4 @@ class ViewController: UIViewController {
         let vc1 =  LiHomeViewController.makeHomeViewController(isSSOLogin: false, ssoToken: nil, deviceToken: nil, notificationProvider: nil)
         self.navigationController?.pushViewController(vc1!, animated: true)
     }
-    
-    
 }
-
