354252f313320547c4b88bc03a0674e9d1918fda
Shekhar Dahore <shekhar.dahore@lithium.com>
2018-10-31 15:33:32 +0530

    AuthService now strongly referenced from AuthManager (#24)
    * AuthService now has a strongly referenced from AuthManager
    
    * removed duplicate code

--- Sources/LiCore/Managers/LiAuthManager.swift
+++ Sources/LiCore/Managers/LiAuthManager.swift
@@ -23,6 +23,7 @@ public class LiAuthManager: NSObject, InternalLiLoginDelegate {
     ///delegate for `LiAuthorizationDelegate`
     weak public var liLoginDelegate: LiAuthorizationDelegate?
     weak var sdkManager: LiSDKManager?
+    var authService: LiAuthService?
     init(sdkManager: LiSDKManager) {
         self.sdkManager = sdkManager
     }
@@ -41,10 +42,9 @@ public class LiAuthManager: NSObject, InternalLiLoginDelegate {
         }
         self.deviceToken = deviceToken
         self.notificationProvider = notificationProvider
-        let authService: LiAuthService
         authService = LiAuthService.init(context: viewController, ssoToken: nil, sdkManager: sdkManager)
-        authService.authDelegate = self
-        authService.startLoginFlow()
+        authService?.authDelegate = self
+        authService?.startLoginFlow()
     }
     /**
      Use this function to initiate sso token based login.
@@ -61,10 +61,9 @@ public class LiAuthManager: NSObject, InternalLiLoginDelegate {
         }
         self.deviceToken = deviceToken
         self.notificationProvider = notificationProvider
-        let authService: LiAuthService
         authService = LiAuthService.init(context: viewController, ssoToken: ssoToken, sdkManager: sdkManager)
-        authService.authDelegate = self
-        authService.startLoginFlow()
+        authService?.authDelegate = self
+        authService?.startLoginFlow()
     }
     /**
      Use this function to check the status of login.
